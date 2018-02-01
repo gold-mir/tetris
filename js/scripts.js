be4e99f45a7b1e6834bb408ddba6645eac1218fb
Tyler Wickline <tyler@>
2018-02-01 09:58:05 -0800

    Add syntax fixes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,288 +1,299 @@
 $(document).ready(function () {
-  var canvas = document.getElementById('canvas');
-  var c = canvas.getContext('2d');
-  startScreen(c, canvas);
-  // drawAll(c, canvas);
-  document.onkeydown = function (e) {
-    switch (e.keyCode) {
-      case 37:
-      moveLeft(c, canvas);
-      break;
-      case 38:
-      rotatePiece(c, canvas);
-      break;
-      case 39:
-      moveRight(c, canvas);
-      break;
-      case 40:
-      moveDown(c, canvas);
-      break;
-      default:
-      console.log("non arrow key");
-    }
-    drawAll(c, canvas);
-    //e.preventDefault();
-  }
+	var canvas = document.getElementById('canvas');
+	var c = canvas.getContext('2d');
+	//startScreen(c, canvas);
+	drawAll(c, canvas);
+	document.onkeydown = function (e) {
+		switch (e.keyCode) {
+		case 37:
+			moveLeft(c, canvas);
+			break;
+		case 38:
+			rotatePiece(c, canvas);
+			break;
+		case 39:
+			moveRight(c, canvas);
+			break;
+		case 40:
+			moveDown(c, canvas);
+			break;
+		default:
+			console.log('non arrow key');
+		}
+		drawAll(c, canvas);
+		//e.preventDefault();
+	};
 
 });
 
 function moveLeft(c, canvas) {
-
-  for (var i = 0; i < inputPiece.points.length; i++) {
-    if (inputPiece.points[i].x <= 0) {
-      return;
-    }
-  }
-  inputPiece = inputPiece.translate(directions.left)
-  console.log(inputPiece);
-  drawScreen(c, canvas);
+	newPiece = inputPiece.translate(directions.left);
+	if (newPiece.collides(bottomBlock) || newPiece.collides(boundingBlock)) {
+		return;
+	}
+	else {
+		inputPiece = inputPiece.translate(directions.left);
+		console.log(inputPiece);
+		drawScreen(c, canvas);
+	}
 }
 
 function moveRight(c, canvas) {
-  for (var i = 0; i < inputPiece.points.length; i++) {
-    if (inputPiece.points[i].x >= 9) {
-      return;
-    }
-  }
-  inputPiece = inputPiece.translate(directions.right)
-  console.log(inputPiece)
-  drawScreen(c, canvas);
+	newPiece = inputPiece.translate(directions.right);
+	if (newPiece.collides(bottomBlock) || newPiece.collides(boundingBlock)) {
+		return;
+	}
+	else {
+		inputPiece = inputPiece.translate(directions.right);
+		console.log(inputPiece);
+		drawScreen(c, canvas);
+	}
 }
 
-function moveDown(c, canvas) {
-  inputPiece = inputPiece.translate(directions.down)
-  console.log(inputPiece)
-  drawScreen(c, canvas);
+function moveDown(c, canvas) { 
+	newPiece = inputPiece.translate(directions.down);
+	if ((newPiece.collides(bottomBlock)) || (newPiece.collides(bottomBlock))) {
+		return;
+	}
+	else {
+		inputPiece = inputPiece.translate(directions.down);
+		console.log(inputPiece);
+		drawScreen(c, canvas);
+	}
 }
 
 function rotatePiece(c, canvas) {
-  inputPiece = inputPiece.rotate();
-  drawAll(c, canvas);
-  return inputPiece;
+	if (inputPiece.rotate().collides(bottomBlock)){
+	    inputPiece = inputPiece.rotate();
+	    drawAll(c, canvas);
+		return inputPiece;
+	}
 }
 
 function startScreen(c, canvas) {
-  var blinkOn = true;
-  var textInteveral = window.setInterval(function () {
-    var image = document.getElementById('source');
-    if (blinkOn) {
-      blinkOn = false;
-      //console.log("Blink On");
-      c.fillStyle = '#555555';
-      c.font = '48px serif';
-      c.fillText('Press Space to begin.', 250, 500);
-      c.drawImage(image, 0, 0);
-    } else {
-      //console.log("Blink Off");
-      blinkOn = true;
-      c.clearRect(0, 0, canvas.width, canvas.height);
-      c.drawImage(image, 0, 0);
-    }
-    document.body.onkeydown = function (e) {
-      if (e.keyCode == 32) {
-        clearInterval(textInteveral);
-        logoFadeInterval = window.setInterval(function () {
-          c.globalAlpha -= 0.01
-          c.clearRect(0, 0, canvas.width, canvas.height);
-          c.drawImage(image, 0, 0);
-          if (c.globalAlpha <= .01) {
-            start = true;
-            clearInterval(logoFadeInterval);
-            c.clearRect(0, 0, canvas.width, canvas.height);
-            c.globalAlpha = 1;
-            //document.body.onkeydown = null;
-            setTimeout(function () {
-              drawAll(c, canvas);
-            }, 1000)
-            //return;
-          }
-        }, 10);
-      }
-    }
-  }, 500);
+	var blinkOn = true;
+	var textInteveral = window.setInterval(function () {
+		var image = document.getElementById('source');
+		if (blinkOn) {
+			blinkOn = false;
+			//console.log("Blink On");
+			c.fillStyle = '#555555';
+			c.font = '48px serif';
+			c.fillText('Press Space to begin.', 350, 600);
+			c.drawImage(image, 0, 0);
+		} else {
+			//console.log("Blink Off");
+			blinkOn = true;
+			c.clearRect(0, 0, canvas.width, canvas.height);
+			c.drawImage(image, 0, 0);
+		}
+		document.body.onkeydown = function (e) {
+			if (e.keyCode == 32) {
+				clearInterval(textInteveral);
+				logoFadeInterval = window.setInterval(function () {
+					c.globalAlpha -= 0.01;
+					c.clearRect(0, 0, canvas.width, canvas.height);
+					c.drawImage(image, 0, 0);
+					if (c.globalAlpha <= .01) {
+						start = true;
+						clearInterval(logoFadeInterval);
+						c.clearRect(0, 0, canvas.width, canvas.height);
+						c.globalAlpha = 1;
+						//document.body.onkeydown = null;
+						setTimeout(function () {
+							drawAll(c, canvas);
+						}, 1000);
+						//return;
+					}
+				}, 10);
+			}
+		};
+	}, 500);
 }
 
 function drawAll(c, canvas) {
 
-  buildCanvas(c, canvas);
-  drawUI(c, canvas);
-  drawScreen(c, canvas);
+	buildCanvas(c, canvas);
+	drawUI(c, canvas);
+	drawScreen(c, canvas);
 }
 
 function buildCanvas(c, canvas) {
-  c.lineWidth = 4;
-  c.beginPath();
-  c.moveTo(2, 2);
-  c.lineTo(506, 2);
-  c.lineTo(506, 1006);
-  c.lineTo(2, 1006);
-  c.closePath();
-  //c.fill();
-  c.stroke();
+	c.lineWidth = 4;
+	c.beginPath();
+	c.moveTo(2, 2);
+	c.lineTo(506, 2);
+	c.lineTo(506, 1006);
+	c.lineTo(2, 1006);
+	c.closePath();
+	//c.fill();
+	c.stroke();
 }
-function generateBlockArr() {
-  blockArr = [];
-  var numPerArr = 4;
-  var blockQuantity = 7;
-  for (var i = 0; i < numPerArr; i++){
-    for (var o = 0; i < blockQuantity; i++) {
+// function generateBlockArr() {
+// 	blockArr = [];
+// 	var numPerArr = 4;
+// 	var blockQuantity = 7;
+// 	for (var i = 0; i < numPerArr; i++){
+// 		for (var o = 0; i < blockQuantity; i++) {
 
-    }
-  }
-}
+// 		}
+// 	}
+// }
 
 function colorPick(color) {
-  var colors = [];
-  switch (color) {
-    case 1:
-    colors = ["#00ffff", "#33ffff", "#00dddd"];
-    break;
-    case 2:
-    colors = ["#0000ff", "#3333ff", "#0000dd"];
-    break;
-    case 3:
-    colors = ["#ff7700", "#ff9933", "#dd6600"];
-    break;
-    case 4:
-    colors = ["#ffff00", "#ffff33", "#dddd00"];
-    break;
-    case 5:
-    colors = ["#00ff00", "#33ff33", "#00dd00"];
-    break;
-    case 6:
-    colors = ["#ff0000", "#ff3333", "#dd0000"];
-    break;
-    case 7:
-    colors = ["#770077", "#993399", "#660066"];
-    break;
-    default:
-    console.log("color out of bounds in colorpick " + color);
-  }
-  return colors;
+	var colors = [];
+	switch (color) {
+	case 1:
+		colors = ['#00ffff', '#33ffff', '#00dddd'];
+		break;
+	case 2:
+		colors = ['#0000ff', '#3333ff', '#0000dd'];
+		break;
+	case 3:
+		colors = ['#ff7700', '#ff9933', '#dd6600'];
+		break;
+	case 4:
+		colors = ['#ffff00', '#ffff33', '#dddd00'];
+		break;
+	case 5:
+		colors = ['#00ff00', '#33ff33', '#00dd00'];
+		break;
+	case 6:
+		colors = ['#ff0000', '#ff3333', '#dd0000'];
+		break;
+	case 7:
+		colors = ['#770077', '#993399', '#660066'];
+		break;
+	default:
+		console.log('color out of bounds in colorpick ' + color);
+	}
+	return colors;
 }
 var inputPiece = pieces.piece3;
 
 function drawScreen(c, canvas) {
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  buildCanvas(c);
-  drawUI(c);
-  for (var i = 0; i < inputPiece.points.length; i++) {
-    drawTile(c, inputPiece.points[i].x, inputPiece.points[i].y, inputPiece.points[i].meta.color);
-
-  }
-
+	c.clearRect(0, 0, canvas.width, canvas.height);
+	buildCanvas(c);
+	drawUI(c);
+	for (var i = 0; i < inputPiece.points.length; i++) {
+		drawTile(c, inputPiece.points[i].x, inputPiece.points[i].y, inputPiece.points[i].meta.color);
+	}
+	for (var o = 0; o < bottomBlock.points.length; o++) {
+		drawTile(c, bottomBlock.points[o].x, bottomBlock.points[o].y, bottomBlock.points[o].meta.color);
+	}
 }
 
 function drawTile(c, x, y, color) {
-  if ((x <= 9) && (y <= 19)) {
+	if ((x <= 9) && (y <= 19)) {
 
-    var xPos = (50 * x) + 5;
-    var yPos = (50 * y) + 5;
-    var colors = colorPick(color);
-    c.lineWidth = 2;
-    c.beginPath();
-    c.moveTo(xPos, yPos);
-    c.lineTo(xPos + 48, yPos);
-    c.lineTo(xPos + 48, yPos + 48);
-    c.lineTo(xPos, yPos + 48);
-    c.closePath();
-    c.stroke();
+		var xPos = (50 * x) + 5;
+		var yPos = (50 * y) + 5;
+		var colors = colorPick(color);
+		c.lineWidth = 2;
+		c.beginPath();
+		c.moveTo(xPos, yPos);
+		c.lineTo(xPos + 48, yPos);
+		c.lineTo(xPos + 48, yPos + 48);
+		c.lineTo(xPos, yPos + 48);
+		c.closePath();
+		c.stroke();
 
-    c.fillStyle = colors[0];
-    c.lineWidth = 1;
-    c.beginPath();
-    c.moveTo(xPos + 2, yPos);
-    c.lineTo(xPos + 48, yPos);
-    c.lineTo(xPos + 48, yPos + 48);
-    c.lineTo(xPos + 2, yPos + 48);
-    c.closePath();
+		c.fillStyle = colors[0];
+		c.lineWidth = 1;
+		c.beginPath();
+		c.moveTo(xPos + 2, yPos);
+		c.lineTo(xPos + 48, yPos);
+		c.lineTo(xPos + 48, yPos + 48);
+		c.lineTo(xPos + 2, yPos + 48);
+		c.closePath();
 
-    c.beginPath();
-    c.moveTo(xPos + 9, yPos + 9);
-    c.lineTo(xPos + 39, yPos + 9);
-    c.lineTo(xPos + 39, yPos + 39);
-    c.lineTo(xPos + 9, yPos + 39);
-    c.closePath();
-    c.fill();
+		c.beginPath();
+		c.moveTo(xPos + 9, yPos + 9);
+		c.lineTo(xPos + 39, yPos + 9);
+		c.lineTo(xPos + 39, yPos + 39);
+		c.lineTo(xPos + 9, yPos + 39);
+		c.closePath();
+		c.fill();
 
-    c.fillStyle = colors[1];
-    c.beginPath();
-    c.moveTo(xPos + 1, yPos + 1);
-    c.lineTo(xPos + 9, yPos + 9);
-    c.lineTo(xPos + 39, yPos + 9);
-    c.lineTo(xPos + 47, yPos + 1);
-    c.closePath();
-    c.fill();
+		c.fillStyle = colors[1];
+		c.beginPath();
+		c.moveTo(xPos + 1, yPos + 1);
+		c.lineTo(xPos + 9, yPos + 9);
+		c.lineTo(xPos + 39, yPos + 9);
+		c.lineTo(xPos + 47, yPos + 1);
+		c.closePath();
+		c.fill();
 
-    //c.fillStyle = '#aaaaaa';
-    c.beginPath();
-    c.moveTo(xPos + 1, yPos + 1);
-    c.lineTo(xPos + 9, yPos + 9);
-    c.lineTo(xPos + 9, yPos + 39);
-    c.lineTo(xPos + 1, yPos + 47);
-    c.closePath();
-    c.fill();
+		//c.fillStyle = '#aaaaaa';
+		c.beginPath();
+		c.moveTo(xPos + 1, yPos + 1);
+		c.lineTo(xPos + 9, yPos + 9);
+		c.lineTo(xPos + 9, yPos + 39);
+		c.lineTo(xPos + 1, yPos + 47);
+		c.closePath();
+		c.fill();
 
-    c.fillStyle = colors[2];
-    c.beginPath();
-    c.moveTo(xPos + 1, yPos + 47);
-    c.lineTo(xPos + 9, yPos + 39);
-    c.lineTo(xPos + 39, yPos + 39);
-    c.lineTo(xPos + 47, yPos + 47);
-    c.closePath();
-    c.fill();
+		c.fillStyle = colors[2];
+		c.beginPath();
+		c.moveTo(xPos + 1, yPos + 47);
+		c.lineTo(xPos + 9, yPos + 39);
+		c.lineTo(xPos + 39, yPos + 39);
+		c.lineTo(xPos + 47, yPos + 47);
+		c.closePath();
+		c.fill();
 
-    c.beginPath();
-    c.moveTo(xPos + 47, yPos + 47);
-    c.lineTo(xPos + 39, yPos + 39);
-    c.lineTo(xPos + 39, yPos + 9);
-    c.lineTo(xPos + 47, yPos + 1);
-    c.closePath();
-    c.fill();
+		c.beginPath();
+		c.moveTo(xPos + 47, yPos + 47);
+		c.lineTo(xPos + 39, yPos + 39);
+		c.lineTo(xPos + 39, yPos + 9);
+		c.lineTo(xPos + 47, yPos + 1);
+		c.closePath();
+		c.fill();
 
-  } else {
-    console.log("Call out of bounds to drawTile function");
-  };
+	} else {
+		console.log('Call out of bounds to drawTile function');
+	}
 }
 var score = 0;
-function drawUI(c, canvas) {
-  c.beginPath();
-  //c.lineWidth = 4;
-  c.fillStyle = '#ccffdd';
-  c.moveTo(556, 2);
-  c.lineTo(860, 2);
-  c.lineTo(860, 206);
-  c.lineTo(556, 206);
-  c.closePath();
-  c.fill();
-  c.stroke();
 
-  c.moveTo(556, 250);
-  c.lineTo(860, 250);
-  c.lineTo(860, 375);
-  c.lineTo(556, 375);
-  c.closePath();
-  c.stroke();
-  c.fillStyle = 'darkblue';
-  c.font='48px Arial';
-  c.textAlign="center";
-  c.fillText ("Score:",708,300);
-  c.fillText (score,708,350);
+function drawUI(c, canvas) {
+	c.beginPath();
+	//c.lineWidth = 4;
+	c.fillStyle = '#ccffdd';
+	c.moveTo(556, 2);
+	c.lineTo(860, 2);
+	c.lineTo(860, 206);
+	c.lineTo(556, 206);
+	c.closePath();
+	c.fill();
+	c.stroke();
+
+	c.moveTo(556, 250);
+	c.lineTo(860, 250);
+	c.lineTo(860, 375);
+	c.lineTo(556, 375);
+	c.closePath();
+	c.stroke();
+	c.fillStyle = 'darkblue';
+	c.font = '48px Arial';
+	c.textAlign = 'center';
+	c.fillText('Score:', 708, 300);
+	c.fillText(score, 708, 350);
 }
 
 function drawNextTile(c, canvas, x, y) {
 
-  if ((x <= 5) && (y <= 3)) {
-    var xPos = (50 * x) + 560;
-    var yPos = (50 * y) + 6;
-    c.beginPath();
-    c.moveTo(xPos, yPos);
-    c.lineTo(xPos + 46, yPos);
-    c.lineTo(xPos + 46, yPos + 46);
-    c.lineTo(xPos, yPos + 46);
-    c.closePath();
-    c.stroke();
-  } else {
-    console.log("Call out of bounds to drawNextTile function");
-  };
+	if ((x <= 5) && (y <= 3)) {
+		var xPos = (50 * x) + 560;
+		var yPos = (50 * y) + 6;
+		c.beginPath();
+		c.moveTo(xPos, yPos);
+		c.lineTo(xPos + 46, yPos);
+		c.lineTo(xPos + 46, yPos + 46);
+		c.lineTo(xPos, yPos + 46);
+		c.closePath();
+		c.stroke();
+	} else {
+		console.log('Call out of bounds to drawNextTile function');
+	}
 }
